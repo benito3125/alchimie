557b38eda1ebf38641235ef72e300052a4060f76
benito3125 <89789800+benito3125@users.noreply.github.com>
2023-04-18 20:15:27 +0200

    MAJ

---
+++ SQL/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE benevoles(
+    ID char(32),
+    first_name char(50),
+    family_name char(50),
+    vege boolean,
+    RJM boolean,
+    RVM boolean,
+    RVS boolean,
+    RSM boolean,
+    RDM boolean
+)
