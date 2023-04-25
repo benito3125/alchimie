dc829fca62def4060ee7a25b6562ef84c2bc0f03
benito3125 <89789800+benito3125@users.noreply.github.com>
2023-04-25 07:57:14 +0200

    Update connect.php

--- code/connect.php
+++ code/connect.php
@@ -3,7 +3,7 @@
 	session_start();
 	// Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
 	if(!isset($_SESSION["username"])){
-		header("Location: login.php");
+		header("Location: index.php");
 		exit(); 
 	}
 ?>
